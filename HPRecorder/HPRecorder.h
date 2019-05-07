e62e5b3270a1e9ed24f94c71bd5415ccf08ba109
Hoang <hoangq@firestak.com>
2019-05-07 23:12:32 +0700

    + Init project

---
+++ HPRecorder/HPRecorder.h
@@ -0,0 +1,19 @@
+//
+//  HPRecorder.h
+//  HPRecorder
+//
+//  Created by Hoang on 5/7/19.
+//  Copyright © 2019 Quang Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HPRecorder.
+FOUNDATION_EXPORT double HPRecorderVersionNumber;
+
+//! Project version string for HPRecorder.
+FOUNDATION_EXPORT const unsigned char HPRecorderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HPRecorder/PublicHeader.h>
+
+
